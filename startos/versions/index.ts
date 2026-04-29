88bd9bdd00ebeaca250b7c986d444005fa276a5e
Aiden McClelland <me@drbonez.dev>
2026-04-28 23:06:33 -0600

    Bump version to 2.2.1:2

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,8 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { v_2_2_1_1 } from './v2.2.1.1'
+import { v_2_2_1_2 } from './v2.2.1.2'
 
 export const versionGraph = VersionGraph.of({
-  current: v_2_2_1_1,
-  other: [],
+  current: v_2_2_1_2,
+  other: [v_2_2_1_1],
 })
